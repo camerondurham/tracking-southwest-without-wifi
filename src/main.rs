7816a09b3f5dde6332d4f6b0bce21495b430c780
Cameron Durham <camdurha@amazon.com>
2023-10-02 18:03:10 -0700

    Print out json

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
-use std::{env, fs, path::PathBuf, error::Error};
+use std::{env, fs, path::PathBuf};
+use serde_json::{Result, Value};
 
-fn main() -> Result<(), Box<dyn Error> > {
+fn main() -> Result<()> {
     let arg_path = env::args().nth(1).expect("No path given");
     let as_path = PathBuf::from(arg_path);
 
-    for entry in fs::read_dir(as_path)? {
-        let entry = entry?;
+    for entry in fs::read_dir(&as_path).expect("unable to read directory") {
+        let entry = entry.expect("cannot unwrap entry");
         let path = entry.path();
-        let metadata = fs::metadata(&path)?;
+        let metadata = fs::metadata(&path).expect("unable to extract metadata");
 
         if metadata.is_file() {
-            println!("parsing file {:?}", path.file_name().expect("no file name found"));
+            let filename = path.file_name().expect("no file name found").to_str().unwrap();
+
+            println!("parsing file {:?}", filename);
+
+            let f_path = PathBuf::from(&as_path).join(filename);
+
+            // TODO: handle errors better here
+            let contents = fs::read_to_string(f_path).expect("cannot read file to string");
+
+            let j: Value = serde_json::from_str(&contents)?;
+            println!("{:?}", j);
         }
     }
 
