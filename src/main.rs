3cca784ee34616430b004eb50068088c40b03e0d
Cameron Durham <camdurha@amazon.com>
2023-10-06 05:57:54 -0700

    Basic de-serde of api response

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{env, fs, path::PathBuf};
 use serde_json::{Result, Value};
+use serde::Deserialize;
 
 fn main() -> Result<()> {
     let arg_path = env::args().nth(1).expect("No path given");
@@ -20,10 +21,56 @@ fn main() -> Result<()> {
             // TODO: handle errors better here
             let contents = fs::read_to_string(f_path).expect("cannot read file to string");
 
-            let j: Value = serde_json::from_str(&contents)?;
-            println!("{:?}", j);
+            //let j: Value = serde_json::from_str(&contents)?;
+            let j: FlightStats = serde_json::from_str(&contents)?;
+            println!("percent complete {:?}, altitude: {:?}", j.percent_flight_complete, j.altitude_value);
         }
     }
 
     Ok(())
+}
+
+/*
+
+    Sample:
+
+    {
+      "sat_commlink_portal": {
+        "status": "conn_ok",
+        "time": "Sat Sep 30 02:28:27 2023"
+      },
+      "pcent_flt_complete": 51,
+      "altVal": "36998",
+      "lon": "-107.664",
+      "satcomm_status": {
+        "commlink": "active",
+        "linkparams": "active"
+      },
+      "dtzone": "CDT",
+      "within_us": true,
+      "etad": "07:00 PM",
+      "lat": "34.387",
+      "gspdVal": "556",
+      "ttgc": "",
+      "dist_remain": "632",
+      "actime24": "21:39"
+    }
+
+*/
+
+#[derive(Debug, Deserialize)]
+pub struct FlightStats {
+    #[serde(alias="pcent_flt_complete")]
+    percent_flight_complete: i32,
+
+    #[serde(alias="altVal")]
+    altitude_value: String,
+
+    #[serde(alias="lon")]
+    longitude: String,
+
+    #[serde(alias="lat")]
+    latitude: String,
+
+    dist_remain: String,
 }
